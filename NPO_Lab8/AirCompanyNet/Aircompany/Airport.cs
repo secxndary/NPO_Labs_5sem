1c8521b8e9743c1bd43b8d88b44beda41260330f
secxndary <valdaitsevv@mail.ru>
2022-11-09 18:27:03 +0300

    git hotfix

---
+++ NPO_Lab8/AirCompanyNet/Aircompany/Airport.cs
@@ -0,0 +1,57 @@
+﻿using Aircompany.Models;
+using Aircompany.Planes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aircompany
+{
+    public class Airport
+    {
+        private List<Plane> _planes;
+
+        public Airport(IEnumerable<Plane> planes) => _planes = planes.ToList();
+
+        public List<Plane> Planes => _planes;
+
+        public List<PassengerPlane> GetPassengerPlanes()
+        {
+            var passengerPlanes = new List<PassengerPlane>();
+            foreach (var plane in _planes)
+                if (plane is PassengerPlane)
+                    passengerPlanes.Add(plane as PassengerPlane);
+            return passengerPlanes;
+        }
+
+        public List<MilitaryPlane> GetMilitaryPlanes()
+        {
+            var militaryPlanes = new List<MilitaryPlane>();
+            foreach (var plane in _planes)
+                if (plane is MilitaryPlane)
+                    militaryPlanes.Add(plane as MilitaryPlane);
+            return militaryPlanes;
+        }
+
+        public List<MilitaryPlane> GetMilitaryPlanesWithType(MilitaryType militaryPlaneType)
+        {
+            var militaryPlanesWithType = new List<MilitaryPlane>();
+            var militaryPlanes = GetMilitaryPlanes();
+
+            foreach (var plane in militaryPlanes)
+                if (plane.Type == militaryPlaneType)
+                    militaryPlanesWithType.Add(plane);
+
+            return militaryPlanesWithType;
+        }
+
+        public PassengerPlane GetPassengerPlaneWithMaxPassengersCapacity() =>
+           GetPassengerPlanes().Aggregate((m, c) => m.PassengersCapacity > c.PassengersCapacity ? m : c);
+
+        public Airport SortByMaxDistance() => new Airport(_planes.OrderBy(p => p.MaxFlightDistance));
+
+        public Airport SortByMaxSpeed() => new Airport(_planes.OrderBy(p => p.MaxSpeed));
+
+        public Airport SortByMaxLoadCapacity() => new Airport(_planes.OrderBy(p => p.MaxLoadCapacity));
+
+        public override string ToString() => $"Airport: planes = {string.Join(", ", _planes.Select(x => x.Model))}\n";
+    }
+}
---
+++ NPO_Lab8/AirCompanyNet/Aircompany/Program.cs
@@ -0,0 +1,53 @@
+﻿using Aircompany.Planes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Aircompany
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //using (StreamWriter sw = new StreamWriter("../../JSON/militaryPlanes.json"))
+            //{
+            //    string json = JsonConvert.SerializeObject(planesPassenger, Formatting.Indented);
+            //    Console.WriteLine(json);
+            //    sw.Write(json);
+            //}
+            //using (var sr = new StreamReader("../../JSON/passengerplanes.json"))
+            //{
+            //    string json = sr.ReadToEnd();
+            //    Console.WriteLine(json);
+            //    planes = JsonConvert.DeserializeObject<List<Plane>>(json);
+            //}
+
+            var planes = new List<Plane>();
+            var passengerPlanes = new List<PassengerPlane>();
+            var militaryPlanes = new List<MilitaryPlane>();
+
+            using (var streamReaderPassengerPlanesJson = new StreamReader("../../JSON/passengerPlanes.json"))
+            {
+                passengerPlanes = JsonConvert.DeserializeObject<List<PassengerPlane>>(streamReaderPassengerPlanesJson.ReadToEnd());
+            }
+            using (var streamReaderMilitaryPlanesJson = new StreamReader("../../JSON/militaryPlanes.json"))
+            {
+                militaryPlanes = JsonConvert.DeserializeObject<List<MilitaryPlane>>(streamReaderMilitaryPlanesJson.ReadToEnd());
+            }
+
+            foreach (var plane in passengerPlanes)
+                planes.Add(plane);
+            foreach (var plane in militaryPlanes)
+                planes.Add(plane);
+
+            var airport = new Airport(planes);
+            var militaryAirport = new Airport(airport.GetMilitaryPlanes());
+            var passengerAirport = new Airport(airport.GetPassengerPlanes());
+
+            Console.WriteLine(militaryAirport.SortByMaxDistance());
+            Console.WriteLine(passengerAirport.SortByMaxSpeed());
+            Console.WriteLine(passengerAirport.GetPassengerPlaneWithMaxPassengersCapacity());
+        }
+    }
+}
